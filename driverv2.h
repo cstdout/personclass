cb2acbe1b4b296af8555244892942e23137befec
cstdout <151808195+cstdout@users.noreply.github.com>
2024-11-29 12:08:01 +0400

    Create driverv2.h

---
+++ driverv2.h
@@ -0,0 +1,23 @@
+#include "person.h"
+class Driver: public Person{
+private:
+    string driverLiscence;
+    int issueDate;
+public:
+    Driver(const string&, const string&, const string&, int, const string&, int);
+    Driver();
+    ~Driver();
+   
+    string getDriverLiscense() const;
+    int getIssueDate() const;
+    
+    void setDriver(const string&, int);
+    void setDriverLiscense(const string&);
+    void setIssueDate(int);
+    
+    int calculateIssueDate() const;
+    
+    friend ostream& operator << (ostream& out, const Driver& dr);
+    friend istream& operator >> (istream& in,  Driver& dr);
+
+};
